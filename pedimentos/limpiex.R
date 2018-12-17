9d3b9f7cd2bceff6e61683e0c26215f4bb47d223
oliabherrera <oliabherrera@gmail.com>
2018-12-17 11:29:49 -0600

    cambios al codigo de limpieza

--- pedimentos/limpiex.R
+++ pedimentos/limpiex.R
@@ -1,51 +1,111 @@
 
 library(readr)
-t501 <- read_delim("datos/t501.txt", "|", 
-                   escape_double = FALSE, trim_ws = TRUE, col_names=FALSE)
-View(t501)
-colnames(t501) <- nombres_501 
+library(readxl)
+library(stringr)
+library(tidyverse)
 
+apendice_1 <- read_excel("apendices/apendice_1.xlsx") %>% 
+  mutate(cve=str_pad(cve,2,pad="0")) %>% 
+  mutate(secc=replace_na(secc,0))%>%
+  mutate(cve=paste0(cve,secc)) %>% 
+  select(cve,valor)
+apendice_2 <- read_excel("apendices/apendice_2.xlsx")
+apendice_3 <- read_excel("apendices/apendice_3.xlsx")
+apendice_4 <- read_excel("apendices/apendice_4.xlsx")
+apendice_5 <- read_excel("apendices/apendice_5.xlsx")
+apendice_10 <- read_excel("apendices/apendice_10.xlsx")
+apendice_14 <- read_excel("apendices/apendice_14.xlsx")
+apendice_15 <- read_excel("apendices/apendice_15.xlsx")
+
+
+####
+
+t501 <- read_delim("datos/noviembre/t501.txt", "|", 
+                   escape_double = FALSE, trim_ws = TRUE, col_names=FALSE)
+colnames(t501) <- nombres_501
+
+
+t501 <- t501 %>% 
+  left_join(apendice_1, by=c("cve_secc_aduanera_despacho"="cve")) %>% 
+  rename(secc_aduanera_despacho=ncol(t501)+1) %>% 
+  mutate(cve_tipo_operacion=ifelse(cve_tipo_operacion==1,"Importacion","Exportacion")) %>%
+  left_join(apendice_2,by=c("cve_documento"="cve")) %>% 
+  rename(documento=ncol(t501)+2) %>% 
+  left_join(apendice_1,by=c("cve_aduana_entrada"="cve")) %>%
+  rename(aduana_entrada=ncol(t501)+3) %>%
+  left_join(apendice_3,by=c("cve_transporte_salida"="cve")) %>% 
+  rename(transporte_salida=ncol(t501)+4) %>% 
+  left_join(apendice_3,by=c("cve_transporte_entrada"="cve")) %>% 
+  rename(transporte_entrada=ncol(t501)+5) %>% 
+  left_join(apendice_3,by=c("cve_medio_transporte"="cve")) %>% 
+  rename(medio_transporte=ncol(t501)+6) %>%
+  left_join(apendice_15,by=c("cve_destino"="cve")) %>% 
+  rename(destino_mercancia=ncol(t501)+7) %>%
+  mutate(cve_tipo_pedimento=ifelse(cve_tipo_pedimento==1,"Normal","Rectificacion"))
 
 
  #######
-t502 <- read_delim("datos/t502.txt", "|", 
+t502 <- read_delim("datos/noviembre/t502.txt", "|", 
                    escape_double = FALSE, trim_ws = TRUE, col_names=FALSE)
 View(t502)
 colnames(t502) <- nombres_502
 
+t502 <-t502 %>% 
+  left_join(apendice_1,by=c("cve_secc_aduanera_despacho"="cve")) %>% 
+  rename(secc_aduanera_despacho=ncol(t502)+1) %>% 
+  left_join(apendice_4,by=c("cve_pais_transporte"="cve")) %>% 
+  rename(pais_transporte=ncol(t502)+2)
+
 
 #####
-t503 <- read_delim("datos/t503.txt", "|", 
+t503 <- read_delim("datos/noviembre/t503.txt", "|", 
                    escape_double = FALSE, trim_ws = TRUE, col_names=FALSE)
-View(t503)
 colnames(t503) <- nombres_503
+View(t503)
 
 
 #####
-t504 <- read_delim("datos/t504.txt", "|", 
+t504 <- read_delim("datos/noviembre/t504.txt", "|", 
                    escape_double = FALSE, trim_ws = TRUE, col_names=FALSE)
-View(t504)
 colnames(t504) <- nombres_504
+View(t504)
+
+t504<-t504 %>% 
+  left_join(apendice_10, by=c("cve_tipo_contenedor"="cve")) %>% 
+  rename(tipo_contenedor=ncol(t504)+1)
 
 
 
 #####
-t505 <- read_delim("datos/t505.txt", "|", 
+t505 <- read_delim("datos/noviembre/t505.txt", "|", 
                    escape_double = FALSE, trim_ws = TRUE, col_names=FALSE)
 View(t505)
 colnames(t505) <- nombres_505
 
+t505<-t505 %>% 
+  left_join(apendice_14,by=c("cve_termino_facturacion"="cve")) %>% 
+  rename(termino_facturacion=ncol(t505)+1) %>% 
+  left_join(apendice_5,by=c("cve_moneda_facturacion"="cve")) %>% 
+  rename(pais_moneda=ncol(t505)+2) %>% 
+  left_join(apendice_4,by=c("cve_pais_facturacion"="cve")) %>% 
+  rename(pais_facturacion=ncol(t505)+3)
 
 
 #####
-t506 <- read_delim("datos/t506.txt", "|", 
+t506 <- read_delim("datos/noviembre/t506.txt", "|", 
                    escape_double = FALSE, trim_ws = TRUE, col_names=FALSE)
 View(t506)
 colnames(t506) <- nombres_506
+t506<-t506 %>% 
+  mutate(tipo_fecha=ifelse(cve_tipo_fecha==1,"Entrada",
+                           ifelse(cve_tipo_fecha==2,"Pago",
+                                ifelse(cve_tipo_fecha==3,"Extraccion",
+                                       ifelse(cve_tipo_fecha==5,"Presentacion",
+                                              ifelse(cve_tipo_fecha==6,"IMP EUA/CAN","ORIGINAL"))))))
 
 
 ##### FALTA
-t507 <- read_delim("datos/t507.txt", "|", 
+t507 <- read_delim("datos/noviembre/t507.txt", "|", 
                    escape_double = FALSE, trim_ws = TRUE, col_names=FALSE)
 View(t507)
 colnames(t507) <- nombres_507
@@ -55,4 +115,18 @@ t509 <- read_delim("datos/t509.txt", "|",
                    escape_double = FALSE, trim_ws = TRUE, col_names=FALSE)
 View(t509)
 colnames(t509) <- nombres_509
+
+#####
+t510 <- read_delim("datos/t510.txt", "|", 
+                   escape_double = FALSE, trim_ws = TRUE, col_names=FALSE)
+View(t510)
+colnames(t510) <- nombres_510
+
+
+#####
+t512 <- read_delim("datos/t512.txt", "|", 
+                   escape_double = FALSE, trim_ws = TRUE, col_names=FALSE)
+View(t512)
+colnames(t512) <- nombres_512
+
 
--- pedimentos/names.R
+++ pedimentos/names.R
@@ -1,8 +1,8 @@
 
 nombres_501<-c("patente","indice","cve_secc_aduanera_despacho","cve_tipo_operacion",
-               "cve_tipo","cve_documento","cve_aduana_entrada","tipo_cambio","total_fletes",
+               "cve_documento","cve_aduana_entrada","tipo_cambio","total_fletes",
                "total_seguros","total_embalajes","total_incrementables","total_otro_deducibles",
-               "peso_bruto","cve_transporte_salida","cve_transporte_entrada","cve_destino",
+               "peso_bruto","cve_transporte_salida","cve_transporte_entrada","cve_medio_transporte","cve_destino",
                "cve_tipo_pedimento","fecha_pago_real")
 
 nombres_502<-c("patente","indice","cve_secc_aduanera_despacho","cve_pais_transporte",
@@ -15,15 +15,46 @@ nombres_504<-c("patente","indice","cve_secc_aduanera_despacho","cve_tipo_contene
                "fecha_pago_real")
 
 nombres_505<-c("patente","indice","cve_secc_aduanera_despacho","fecha_facturacion",
-               "cve_moneda_facturacion","valor_dolares","valor_moneda_extranjera",
+               "cve_termino_facturacion","cve_moneda_facturacion","valor_dolares","valor_moneda_extranjera",
                "cve_pais_facturacion","cve_entidad_facturacion","fecha_balanza","fecha_pago_real")
 
 nombres_506<-c("patente","indice","cve_secc_aduanera_despacho","cve_tipo_fecha", "fecha_operacion",
                "fecha_pago_real")
 
-nombres_507<-c("patente","indice","cve_secc_aduanera_despacho","cve_tipo_fecha", "fecha_operacion",
+nombres_507<-c("patente","indice","cve_secc_aduanera_despacho","cve_caso", "id_caso",
                "fecha_pago_real")
 
-nombres_509<-c("patente","indice","cve_secc_aduanera_despacho","cve_contribucion", "cve_forma_pago",
+nombres_509<-c("patente","indice","cve_secc_aduanera_despacho","cve_contribucion", "tasa_contribucion",
+               "cve_tipo_tasa","fecha_pago_real")
+
+nombres_510<-c("patente","indice","cve_secc_aduanera_despacho","cve_contribucion", "cve_forma_pago",
                "importe_pago","fecha_pago_real")
+
+nombres_512<-c("patente","indice","cve_secc_aduanera_despacho","patente_aduana_original", "indice_original",
+               "cve_secc_aduanera_despacho_original","cve_documento_original","fecha_operacion_original",
+               "fraccion_arancelaria_original","cve_unidad_medida_original","cantidad_mercancia_descargada",
+               "fecha_pago_real")
+
+nombres_520<-c("patente","indice","cve_secc_aduanera_despacho","cve_pais_domicilio_dest", "fecha_pago_real")
+
+
+nombres_551<-c("patente","indice","cve_secc_aduanera_despacho","fraccion_arancelaria","sec_fraccion_arancelaria"
+               , "sub_fraccion_arancelaria",
+               "descripcion_mercancia","precio_unitario","valor_aduana","valor_comercial","valor_dolares",
+               "cantidad_mercancia_unidades_medida_comercial","cve_unidad_medida_comercial",
+               "cantidad_mercancia_unidades_medida_tarifaria",
+               "cve_unidad_medida_tarifaria","valor_agregado","cve_vinculacion","cve_metodo_valorizacion",
+               "codigo_mercancia","marca_mercancia","modelo_mercancia","cve_pais_origen","cve_pais_comprador",
+               "cve_entidad_origen","cve_entidad_destino","cve_entidad_comprador","cve_entidad_vendedor","fecha_pago_real")
+
+nombres_552<-c("patente","indice","cve_secc_aduanera_despacho","fraccion_arancelaria","sec_fraccion_arancelaria",
+               "km_vehiculo","fecha_pago_real")
+
+
+
+
+
+
+
+
 
